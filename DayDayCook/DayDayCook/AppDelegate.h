da094fb78ccb538569f169075dc61dd64f6ffd91
jiang <18862241437@163.com>
2017-03-20 22:25:02 +0800

    初始化

---
+++ DayDayCook/DayDayCook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DayDayCook
+//
+//  Created by jiang on 2017/3/20.
+//  Copyright © 2017年 蒋玉顺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
